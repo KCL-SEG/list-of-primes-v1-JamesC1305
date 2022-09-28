8e2f81af59c23356b7d1355742e9ea672a6f49ae
James Curtis <james.curtis@kcl.ac.uk>
2022-09-28 17:51:21 +0100

    Add number of primes feature

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    n = 3
+    while len(list) < number_of_primes:
+        if n % 2 != 0:
+            f = 3
+            while f * f <= n:
+                if n % f == 0:
+                    break
+                else:
+                    f += 2
+            else:
+                list.append(n)
+        n += 1
     return list
+
+print(primes(2))
